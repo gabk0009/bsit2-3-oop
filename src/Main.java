7b5f196c022e9d6e95595244e832ed0b29b7af24
Kent Gabe <kgabe46585@liceo.edu.ph>
2025-08-18 15:35:22 +0800

    lab-activity-4

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    System.out.println("Hello world");
+        GradeCalculator calculator = new GradeCalculator();
 
-        }
+        String studentName = "John Smith";
+        double[] grades = {85.5, 92.0, 78.5, 90.0};
 
+        double average = calculator.calculateAverage(grades);
+
+
+        String letterGrade = calculator.getLetterGrade(average);
+
+        calculator.displayResult(studentName, average);
+        System.out.println();
+        calculator.displayResult(studentName, average, letterGrade);
+    }
 }
