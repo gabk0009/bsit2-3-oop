e737324fdd8b06c858f1f9635ac4ab93af30c877
Kent Gabe <kgabe46585@liceo.edu.ph>
2025-09-23 18:44:21 +0800

    Update Pet.java

--- src/Pet.java
+++ src/Pet.java
@@ -1,1 +1,67 @@
+abstract class Pet {
+    String name;
+    int age;
 
+    public Pet(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public abstract void makeSound();
+
+    public void displayInfo() {
+        System.out.println("Pet Name: " + name + ", Age: " + age);
+    }
+}
+class Bird extends Pet implements Trainable {
+
+    public Bird(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " says: Tweet! Tweet!");
+    }
+
+    @Override
+    public void performTrick() {
+        System.out.println("Training " + name + ": Flies in circles and lands on perch");
+    }
+}
+class Cat extends Pet {
+
+    public Cat(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " says: Meow! Meow!");
+    }
+}
+class Dog extends Pet implements Trainable {
+
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " says: Woof! Woof!");
+    }
+
+    @Override
+    public void performTrick() {
+        System.out.println("Training " + name + ": Sits and shakes hands");
+    }
+}
+interface Trainable {
+    void performTrick();
+
+    static void startTrainingSession() {
+        System.out.println("Training Session Started!");
+        System.out.println("===========================");
+        System.out.println();
+    }
+}
