9a259fe17852def45faed3731b9e97d503e63958
Kent Gabe <kgabe46585@liceo.edu.ph>
2025-10-02 15:49:25 +0800

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,1 +1,38 @@
 
+class Book extends LibraryItem {
+    private String isbn;
+    private int numberOfPages;
+    private String genre;
+
+    public Book(String itemId, String title, String author, String isbn, int numberOfPages, String genre) {
+        super(itemId, title, author);
+        this.isbn = isbn;
+        this.numberOfPages = numberOfPages;
+        this.genre = genre;
+    }
+
+    public String getItemType() {
+        return "Book";
+    }
+
+    public double calculateLateFee(int daysLate) {
+        return daysLate * 0.50;
+    }
+
+    public void borrowItem(String borrowerName) {
+        checkOut(borrowerName);
+    }
+
+    public void returnItem() {
+        checkIn();
+    }
+
+    public boolean isAvailable() {
+        return !isCheckedOut;
+    }
+
+    public int getBorrowingPeriod() {
+        return 14; 
+    }
+}
+
