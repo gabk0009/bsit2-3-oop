7841f6d0bbe161e9008d6f48a47f7e80d91a0045
Kent Gabe <kgabe46585@liceo.edu.ph>
2025-10-02 15:49:47 +0800

    Update LibraryItem.java

--- src/LibraryItem.java
+++ src/LibraryItem.java
@@ -1,1 +1,30 @@
+abstract class LibraryItem implements Borrowable {
+    protected String itemId;
+    protected String title;
+    protected String author;
+    protected boolean isCheckedOut = false;
+    protected String borrowerName = "";
 
+    public LibraryItem(String itemId, String title, String author) {
+        this.itemId = itemId;
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getItemInfo() {
+        return title + " (" + getItemType() + " - " + getBorrowingStatus() + ")";
+    }
+
+    public void checkOut(String borrowerName) {
+        isCheckedOut = true;
+        this.borrowerName = borrowerName;
+    }
+
+    public void checkIn() {
+        isCheckedOut = false;
+        this.borrowerName = "";
+    }
+
+    public abstract String getItemType();
+    public abstract double calculateLateFee(int daysLate);
+}
