4bdef4ea8c514787e7e167e6086367a826651035
Kent Gabe <kgabe46585@liceo.edu.ph>
2025-10-02 15:46:34 +0800

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,1 +1,33 @@
+import java.util.ArrayList;
+abstract class User {
+    protected String userId;
+    protected String name;
+    protected String email;
+    protected ArrayList<LibraryItem> borrowedItems = new ArrayList<>();
 
+    public User(String userId, String name, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public void addBorrowedItem(LibraryItem item) {
+        borrowedItems.add(item);
+    }
+
+    public void removeBorrowedItem(LibraryItem item) {
+        borrowedItems.remove(item);
+    }
+
+    public void displayBorrowedItems() {
+        for (LibraryItem item : borrowedItems) {
+            System.out.println(item.getItemInfo());
+        }
+    }
+
+    public int getBorrowedItemsCount() {
+        return borrowedItems.size();
+    }
+
+    public abstract int getMaxBorrowLimit();
+}
