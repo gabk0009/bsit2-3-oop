15cd115ecdbcbb5a99fb5a6fb78019e9b272eaf6
Kent Gabe <kgabe46585@liceo.edu.ph>
2025-10-02 15:50:22 +0800

    Update DVD.java

--- src/DVD.java
+++ src/DVD.java
@@ -1,1 +1,41 @@
+class DVD extends LibraryItem {
+    private int duration;
+    private String rating;
+    private String genre;
+
+    public DVD(String itemId, String title, String author, int duration, String rating, String genre) {
+        super(itemId, title, author);
+        this.duration = duration;
+        this.rating = rating;
+        this.genre = genre;
+    }
+
+    public String getItemType() {
+        return "DVD";
+    }
+
+    public double calculateLateFee(int daysLate) {
+        return daysLate * 1.00;
+    }
+
+    public void borrowItem(String borrowerName) {
+        checkOut(borrowerName);
+    }
+
+    public void returnItem() {
+        checkIn();
+    }
+
+    public boolean isAvailable() {
+        return !isCheckedOut;
+    }
+
+    public int getBorrowingPeriod() {
+        return 5; 
+    }
+
+    public String getBorrowingStatus() {
+        return "DVD: " + super.getBorrowingStatus();
+    }
+}
 
