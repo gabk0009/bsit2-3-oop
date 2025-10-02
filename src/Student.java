9da89c4eaa202d693b194bce4e31ce298faef5c2
Kent Gabe <kgabe46585@liceo.edu.ph>
2025-10-02 15:46:53 +0800

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,1 +1,16 @@
+class Student extends User {
+    private String studentId;
+    private String major;
+
+    public Student(String userId, String name, String email, String studentId, String major) {
+        super(userId, name, email);
+        this.studentId = studentId;
+        this.major = major;
+    }
+
+    public int getMaxBorrowLimit() {
+        return 5;
+    }
+}
+
 
