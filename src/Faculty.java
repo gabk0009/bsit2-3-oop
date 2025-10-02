9aad44b1d6f1f6f9490b272c12b7f91484f4a1cd
Kent Gabe <kgabe46585@liceo.edu.ph>
2025-10-02 15:52:45 +0800

    Update Faculty.java

--- src/Faculty.java
+++ src/Faculty.java
@@ -1,1 +1,14 @@
+class Faculty extends User {
+    private String department;
+    private String position;
 
+    public Faculty(String userId, String name, String email, String department, String position) {
+        super(userId, name, email);
+        this.department = department;
+        this.position = position;
+    }
+
+    public int getMaxBorrowLimit() {
+        return 10;
+    }
+}
