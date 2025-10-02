c9f0a7ba13b8963cefc597e7b72bb87b41f4cad8
Kent Gabe <kgabe46585@liceo.edu.ph>
2025-10-02 15:45:25 +0800

    Update Borrowable.java

--- src/Borrowable.java
+++ src/Borrowable.java
@@ -1,1 +1,19 @@
+interface Borrowable {
+    void borrowItem(String borrowerName);
+    void returnItem();
+    boolean isAvailable();
+    int getBorrowingPeriod(); // In days
+    default String getBorrowingStatus() {
+        return isAvailable() ? "Available for borrowing" : "Currently borrowed";
+    }
+}
+
+
+
+
+
+
+
+
+
 
